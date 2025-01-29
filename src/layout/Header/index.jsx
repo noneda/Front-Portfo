b47a4d7dc990d5c4e71c86c178a5c57ec5bca335
noneda <nonejda@gmail.com>
2025-01-28 22:23:38 -0500

    Pssss p Fixed

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <>
       <header>
         <div className="title">
-          <h1 className="tiny5-regular">
+          <h1 className="barrio-regular">
             <b>Noneda´s</b> <p className="barrio-regular">Portfolio!!</p>
           </h1>
         </div>
@@ -13,17 +13,15 @@ const Header = () => {
           <picture className="Profile">
             <img src="/ewrwfsgaghaha.jpeg" alt="a" />
           </picture>
-          <h4 className="delius-regular">
-            <p>
-              I am a Software Engineering student with a strong passion for web
-              development and a solid commitment to continuous learning. I am
-              looking for an opportunity to do my internship in a professional
-              environment where I can apply and expand my technical knowledge,
-              contributing with my skills and desire to learn. I am motivated to
-              take on challenges and quickly adapt to new technologies, bringing
-              a proactive and solution-oriented mindset.
-            </p>
-          </h4>
+          <p className="delius-regular">
+            I am a Software Engineering student with a strong passion for web
+            development and a solid commitment to continuous learning. I am
+            looking for an opportunity to do my internship in a professional
+            environment where I can apply and expand my technical knowledge,
+            contributing with my skills and desire to learn. I am motivated to
+            take on challenges and quickly adapt to new technologies, bringing a
+            proactive and solution-oriented mindset.
+          </p>
         </div>
       </header>
     </>
