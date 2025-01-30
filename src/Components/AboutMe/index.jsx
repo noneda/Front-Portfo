ce6a10b4ef8bc9fe4a65929230ab6c74ca7a993c
noneda <nonejda@gmail.com>
2025-01-29 23:28:58 -0500

    About Me grid In Progress

--- src/Components/AboutMe/index.jsx
+++ src/Components/AboutMe/index.jsx
@@ -4,7 +4,33 @@ const AboutMe = () => {
   return (
     <>
       <div className="AboutMe">
-        <div>Wenas</div>
+        <div className="MakesYouDifferent">
+          Soy Juan David Pardo Forero, estudiante de Ingeniería de Software con
+          una gran pasión por la programación y la exploración de nuevas
+          tecnologías. Lo que más me motiva de este campo es la posibilidad de
+          plasmar ideas en software, creando soluciones innovadoras sin límites
+          más allá de la creatividad y el pensamiento crítico. Me encanta
+          enfrentar desafíos, aprender desde la base y comprender a fondo cada
+          tecnología con la que trabajo.
+        </div>
+        <div className="YouProveIt">
+          Me destaco por ser una persona curiosa, autodidacta y con una fuerte
+          orientación a la resolución de problemas. Disfruto de los retos
+          tecnológicos y siempre busco optimizar soluciones en cada proyecto.
+          Aunque actualmente mi objetivo es convertirme en un desarrollador
+          full-stack, tengo un interés particular por el desarrollo de bajo
+          nivel y la ciberseguridad.
+        </div>
+        <div className="WantToAchieve">
+          A corto plazo, mi meta es conseguir una pasantía en una organización
+          enfocada en desarrollo de software donde pueda seguir aprendiendo,
+          mejorar mis habilidades y adquirir nuevos puntos de vista en la
+          industria. Además, quiero profundizar en Deep Learning y explorar su
+          aplicación en diferentes áreas. A largo plazo, aspiro a ser un
+          desarrollador full-stack y contribuir activamente a proyectos Open
+          Source, ya que creo en el poder de la comunidad y el aprendizaje
+          colaborativo.
+        </div>
       </div>
     </>
   );
