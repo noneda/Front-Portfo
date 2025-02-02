8c78901b276ed950f4d37edddc00c5b28e19c72e
noneda <nonejda@gmail.com>
2025-02-02 18:03:17 -0500

    About...

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -2,18 +2,70 @@ import "./base.css";
 const App = () => {
   return (
     <div className="App">
-      <div className="AboutMe">
-        <h2>
-          <b>About Me</b> 🚀
-        </h2>
+      <div className="Section">
+        <div className="head">
+          <h2>
+            <b>About Me</b>
+          </h2>
+          <picture>
+            <img src="/Me.jpeg" />
+          </picture>
+        </div>
         <p>
-          Hi! I'm <b>Juan David Pardo Forero</b>, a Software Engineering student
-          passionate about web development, cybersecurity, and low-level
-          programming. I enjoy solving challenges, learning new technologies,
-          and optimizing systems. I'm seeking an internship to apply
-          my knowledge and grow in a professional environment. I believe
-          technology has no limits—only those set by our creativity and
-          problem-solving mindset. 💡🔥
+          Hi! I'm <b>Juan David Pardo Forero</b> 👨‍💻, a Software Engineering
+          student 🎓 from Bogotá Colombia, <br />
+          Passionate about web development 🌐, cybersecurity 🔒, and low-level
+          programming⚙️.
+          <br />
+          I enjoy solving challenges🧩, learning new technologies📚, and
+          optimizing systems 🚀. <br />
+          I'm seeking an internship 💼 to apply my knowledge and grow in a
+          professional environment🏢. <br />I believe technology has no
+          limits—only 🚧 those set by our creativity 🎨 and problem-solving
+          mindset. 🧠.
+        </p>
+      </div>
+      <div className="Section">
+        <div className="head">
+          <h2>Education 🎓📚</h2>
+        </div>
+        <p>
+          <b>Universitaria de Colombia 🖥️</b> - Bogotá, Colombia <br />
+          Bachelor's in Software Engineering <br />
+          Expected Graduation: 2026 🎓
+          <br />
+          <ul>
+            <li>
+              Focus on software development 💻, algorithms 🧩, data structures
+              📊, and system architecture🏗️.
+            </li>
+            <li>
+              Actively learning technologies such as React⚛️, 🐍, C++ 🖥️, and
+              Deep Learning 🤖
+            </li>
+          </ul>
+          <br />
+          <b>SENA</b> - Mosquera, Colombia <br />
+          Software Programming Technician 🖥️
+          <br />
+          Graduated: 2021 🎓
+          <br />
+          <ul>
+            <li>
+              Solid foundation in software programming 📝, problem-solving 🔍,
+              and system design 🏗️
+            </li>
+          </ul>
+          <br />
+          <b>High School</b> - Cota - Cundinamarca <br />
+          Colombia Graduated: 2021 🎓
+          <br />
+          <ul>
+            <li>
+              Strong foundation in mathematics ➗, programming 👨‍💻, and
+              problem-solving 🧠.
+            </li>
+          </ul>
         </p>
       </div>
     </div>
