6e5531c0d51abbd0b75fa72b38aea70a37674458
noneda <nonejda@gmail.com>
2025-02-03 16:13:43 -0500

    Colores de Mierda Q ESTRESSSSSSSs

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -25,46 +25,51 @@ const App = () => {
           mindset. 🧠.
         </p>
       </div>
-      <div className="Section">
-        <h2>Education 🎓📚</h2>
+      <div className="Section Education">
+        <h2>Education 🎓</h2>
         <p>
           <b>Universitaria de Colombia 🖥️</b> - Bogotá, Colombia <br />
           Bachelor's in Software Engineering <br />
           Expected Graduation: 2026 🎓
-          <br />
-          <ul>
-            <li>
-              Focus on software development 💻, algorithms 🧩, data structures
-              📊, and system architecture🏗️.
-            </li>
-            <li>
-              Actively learning technologies such as React⚛️, 🐍, C++ 🖥️, and
-              Deep Learning 🤖
-            </li>
-          </ul>
-          <br />
+        </p>
+        <br />
+        <ul>
+          <li>
+            Focus on software development 💻, algorithms 🧩, data structures 📊,
+            and system architecture🏗️.
+          </li>
+          <li>
+            Actively learning technologies such as React⚛️, 🐍, C++ 🖥️, and Deep
+            Learning 🤖
+          </li>
+        </ul>
+        <br />
+        <p>
           <b>SENA</b> - Mosquera, Colombia <br />
           Software Programming Technician 🖥️
           <br />
           Graduated: 2021 🎓
-          <br />
-          <ul>
-            <li>
-              Solid foundation in software programming 📝, problem-solving 🔍,
-              and system design 🏗️
-            </li>
-          </ul>
-          <br />
+        </p>
+        <ul>
+          <li>
+            Solid foundation in software programming 📝, problem-solving 🔍, and
+            system design 🏗️
+          </li>
+        </ul>
+        <br />
+        <p>
           <b>High School</b> - Cota - Cundinamarca <br />
           Colombia Graduated: 2021 🎓
-          <br />
-          <ul>
-            <li>
-              Strong foundation in mathematics ➗, programming 👨‍💻, and
-              problem-solving 🧠.
-            </li>
-          </ul>
         </p>
+        <ul>
+          <li>
+            Strong foundation in mathematics ➗, programming 👨‍💻, and
+            problem-solving 🧠.
+          </li>
+        </ul>
+      </div>
+      <div className="Section Technologies">
+
       </div>
     </div>
   );
